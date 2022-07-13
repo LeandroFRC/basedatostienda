c8ef7ddd0c1762e39b0c6d42854a96c566fd2e90
Leandro <leitoriv@gmail.com>
2022-07-13 14:44:12 -0400

    programa sin F

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://leandrorivera:leitoriv2003@cluster0.y4lwe.mongodb.net/tienda?retryWrites=true&w=majority";
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://leandrorivera:Lrc2003$@cluster0.y4lwe.mongodb.net/?retryWrites=true&w=majority";
